28ec86b939121dd14e4487be3b8d0ce2a5beba63
Max <max.kononerov@gmail.com>
2025-06-07 01:50:32 +0400

    First step

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import {defineProps} from 'vue'
+    
+const props = defineProps<{send: string}>();
+
+    const newCount = ref(0);
+
+</script>
+
+<template>
+
+    <div>
+        <p>{{ props.send }}</p>
+
+        <p>Счетчик - {{ newCount }}</p>
+
+        <button @click="newCount++">+</button>
+
+        <button @click="newCount--">-</button>
+
+
+    </div>
+</template>
+
+<style>
+
+</style>
